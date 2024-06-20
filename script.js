515fb584ad3b83dd94a557d1b52c18a9bcac4053
lemoncat0817 <king123456789abc@gmail.com>
2024-06-21 06:27:38 +0800

    首頁 以及 作品集頁面 完成

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $('.gotoPOS').click(function () {
+    window.location.href = 'https://lemoncat0817.github.io/Vue3-POS';
+  });
+
+  $('.gotoWeather').click(function () {
+    window.location.href = 'https://lemoncat0817.github.io/Vue3-WeatherWeb';
+  });
+  $('.gotoLohas').click(function () {
+    window.location.href = 'https://lemoncat0817.github.io/Vue3-PetCafe/';
+  });
+  $('.gotoTodo').click(function () {
+    window.location.href = 'https://lemoncat0817.github.io/Vue-TodoList/';
+  });
+})
